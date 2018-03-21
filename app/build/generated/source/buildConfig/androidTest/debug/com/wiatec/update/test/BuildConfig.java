57c3277a7f95e02c0b52da8ba67a1a971a5b8361
BlueDimension <7241368@qq.com>
2018-03-21 15:16:32 +0800

    201803211515

--- app/build/generated/source/buildConfig/androidTest/debug/com/wiatec/update/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/wiatec/update/test/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.wiatec.update.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 402;
-  public static final String VERSION_NAME = "v4.0.2-debug";
+  public static final int VERSION_CODE = 403;
+  public static final String VERSION_NAME = "v4.0.3-debug";
   // Fields from build type: debug
   public static final boolean LOG_DEBUG = true;
 }
